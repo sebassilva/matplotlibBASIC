33e551c29eb33aad54055dc76e20261bffda5027
sebassilva <seebassilva@gmail.com>
2016-05-24 21:45:39 -0500

    Versión entregable
    Se agregaron comentarios.
    Ahora se pueden graficar tantas URLs como el usuario lo desee.
    Se validan casos de uso.

--- ejemplos/urlResponse.py
+++ ejemplos/urlResponse.py
@@ -1,45 +1,66 @@
+#Universidad Nacional Autónoma de México
+#Facultad de Ingeniería
+#Ingeniería en Computación
+#Estructuras de Datos y Algoritmos
+#UrlTimePlot.py
+#Creado por:
+#Aguilera, Tania
+#López, Gerardo
+#Silva, Sebastián
+#Licencia de Software: MIT
+
+#Si se llegara a presentar el error: "unknown locale UTF-8", copiar y pegar esta línea en la consola:
+# "echo -e "export LC_ALL=en_US.UTF-8\nexport LANG=en_US.UTF-8" >> ~/.bashrc && source ~/.bashrc"
+#Importa las librerías necesarias para su funcionamiento
 import urllib.request
 from time import time
-
-timeArray=[]
-i=0
-def responseTime(urlToParse):
-    t0 = time()
-    partial=0;
-    response = urllib.request.urlopen(urlToParse).read()
-    partial= time()-t0
-    print("Tiempo "+ format(partial) + " de: "+ urlToParse)
-    timeArray.append(partial)
-  
-
-print("Function:")
-responseTime("http://profesores.fi-b.unam.mx/carlos/eda1/fuerza_bruta.py")
-responseTime("http://google.com")
-responseTime("http://www.facebook.com/")
-responseTime("http://www.unam.mx/")
-responseTime("http://gadmaweb.com/")
-responseTime("http://www.gigigo.com/app/es/home")
-
-
 from matplotlib import pyplot as plt
 from matplotlib import style
+#Se definen los arreglos en el que se almacenan los tiempos y las URLS, así como variables globales necesarias
+timeArray = []
+urlArray = []
+otherUrl = True
+i=0
 
-style.use('ggplot')
+def responseTime(urlToParse):
+	#Obtiene el tiempo que se tarda en obtener respuesta y parsear su contenido de un servidor
+    global i
+    i+=1
+    t0 = time()
+    partial=0;
+    #Hace la petición y lee el contenido de la respuesta
+    response = urllib.request.urlopen(urlToParse).read()
+    partial= time()-t0
+    print("Tiempo: "+ format(partial) + " de "+ urlToParse)
+    #Agrega el tiempo al arreglo de tiempos
+    timeArray.append(partial)
+    urlArray.append(i)
+  
 
-x = [1,3,5,7,9,11]
-y = timeArray
-
-#x2 = [2,4,6]
-#y2 = [6,15,7]
-
-
-plt.bar(x, y, align='center')
-
-#plt.bar(x2, y2, color='g', align='center')
-
-
-plt.title('Response time')
-plt.ylabel('Time')
-plt.xlabel('Url')
-
-plt.show()
+while otherUrl is True: 
+	#Pide tantas direcciones como se quieran comparar
+	url = input("Ingresa una dirección a comparar: ")
+	if url: 
+		try: 
+			#Si se ingresó una cadena válida
+			responseTime(url)
+			print(otherUrl)
+		except ValueError:
+			print("Ingresa una dirección válida")
+		otherUrl = True
+	else: 
+		#Si no se ingresó ninguna URL
+		if len(urlArray) > 1: 
+			otherUrl = False
+			style.use('ggplot')
+			x = urlArray
+			y = timeArray
+			plt.bar(x, y, align='center')
+			plt.title('Tiempo de respuesta')
+			plt.ylabel('Tiempo [s]')
+			plt.xlabel('# de url')
+			#imprime la gráfica
+			plt.show()
+			break
+		else: 
+			print("Ingresa al menos una dirección.")
